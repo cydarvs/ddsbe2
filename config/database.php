e0e5082d8ffaf4a7d57382f4a05136bc317f7630
Cyfred Odarve <cyfredodarve@gmail.com>
2021-05-27 12:50:46 +0800

    Balik the database config

--- config/database.php
+++ config/database.php
@@ -32,8 +32,10 @@ return [
     */
 
     'migrations' => 'migrations',
+
     'connections' => [
         // edit the database.php
+        /*
         'mysql' => [
             'driver'        => 'mysql',
             'host'          => env('DB_HOST', 'remotemysql.com'),
@@ -49,7 +51,23 @@ return [
             // 'engine'        => env('DB_ENGINE', null),
             // 'timezone'      => env('DB_TIMEZONE', '+00:00'),
         ],
-
+        */
+        'mysql' => [
+            'driver'        => 'mysql',
+            'host'          => env('DB_HOST', null),
+            'port'          => env('DB_PORT', null),
+            'database'      => env('DB_DATABASE', null),
+            'username'      => env('DB_USERNAME', null),
+            'password'      => env('DB_PASSWORD', null),
+            'unix_socket'   => env('DB_SOCKET', ''),
+            'charset'       => env('DB_CHARSET', 'utf8'),
+            'collation'     => env('DB_COLLATION', 'utf8_unicode_ci'),
+            'prefix'        => env('DB_PREFIX', ''),
+            // 'strict'        => env('DB_STRICT_MODE', false),
+            // 'engine'        => env('DB_ENGINE', null),
+            // 'timezone'      => env('DB_TIMEZONE', '+00:00'),
+        ],
+        
         'sqlsrv' => [
             'driver'        => 'sqlsrv',
             'host'          => env('DB_HOST1', null),
